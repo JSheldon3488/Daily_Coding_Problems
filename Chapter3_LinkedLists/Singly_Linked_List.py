6d58ff7aadd218b9208282bf9bab405cda883863
Justin Sheldon <JSheldon3488@gmail.com>
2019-07-02 15:43:43 -0500

    Added __str__ for Nodes
    Needed a better visual representation for a single node to check answers in problem 3.4

--- Chapter3_LinkedLists/Singly_Linked_List.py
+++ Chapter3_LinkedLists/Singly_Linked_List.py
@@ -11,6 +11,9 @@ class Node(object):
         self.data = data
         self.next = next
 
+    def __str__(self):
+        return "[" + str(self.data) + "]"
+
 
 class Singly_Linked_List(object):
     """
