98e032f308fbfa856da3de3e5130ceacc2231b60
Justin Sheldon <JSheldon3488@gmail.com>
2020-07-07 13:47:26 -0500

    Add files via upload

---
+++ Template.py
@@ -0,0 +1,28 @@
+"""
+Date: 6/14/19
+2.2: Generate Palindrome Pairs
+"""
+
+'''
+Problem Statement: 
+
+'''
+
+'''                             My Solution                              '''
+
+
+'''                             Book Solution                            '''
+
+
+'''                             Test Cases                               '''
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
+
+'''
+Lessons Learned:
+    *
+'''
+
