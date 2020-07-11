58ee37d3770d7597c89cc657f12965fba28b315d
Justin Sheldon <JSheldon3488@gmail.com>
2020-07-11 11:17:14 -0500

    Implemented BST for Chapter 7

---
+++ Chapter7_Binary_Search_Trees/BST.py
@@ -0,0 +1,44 @@
+class Node:
+    """ Node class used for nodes in a Tree """
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.right = right
+        self.left = left
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, x):
+        if not self.root:
+            self.root = Node(x)
+        else:
+            self._insert(x, self.root)
+
+    def _insert(self, x, root):
+        if x < root.data:
+            if not root.left:
+                root.left = Node(x)
+            else:
+                self._insert(x, root.left)
+        else:
+            if not root.right:
+                root.right = Node(x)
+            else:
+                self._insert(x, root.right)
+
+    def find(self, x):
+        if not self.root:
+            return False
+        else:
+            return self._find(x, self.root)
+
+    def _find(self, x, root):
+        if not root:
+            return False
+        elif x == root.data:
+            return True
+        elif x < root.data:
+            return self._find(x, root.left)
+        else:
+            return self._find(x, root.right)
