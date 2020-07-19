287f6739482d0e48a709387e1062e15d0a3d8bd8
Justin Sheldon <JSheldon3488@gmail.com>
2020-07-19 15:04:40 -0500

    Completed: Trie Data Structure

---
+++ Chapter8_Tries/8.1_autocomplete_system.py
@@ -0,0 +1,32 @@
+"""
+Date: 7/19/20
+8.1 Implement autocomplete system
+"""
+
+'''
+Problem Statement: Implement an autocomplete system. That is, given a query string s and a set of all possible query
+strings, return all strings in the set that have s as a prefix.
+Example:
+    query string: "de", set of strings ["dog", "deer", "deal"]
+    returns ["deer", "deal"]
+
+'''
+
+'''                             My Solution                              '''
+
+
+'''                             Book Solution                            '''
+
+
+'''                             Test Cases                               '''
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
+
+'''
+Lessons Learned:
+    *
+'''
+
