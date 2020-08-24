e34f1021897fc6ea1343b3c82ad6f51cb95a9e65
Justin Sheldon <JSheldon3488@gmail.com>
2020-08-24 14:43:28 -0500

    Completed: 14.1

--- Chapter14_Backtracking/14.1_Compute_Flight_Itinerary.py
+++ Chapter14_Backtracking/14.1_Compute_Flight_Itinerary.py
@@ -4,7 +4,7 @@ Date: 8/24/20
 """
 
 '''
-Problem Statement: Given an unordered list of flights taken by someone,ach represented as (origin, destination) pairs,
+Problem Statement: Given an unordered list of flights taken by someone, each represented as (origin, destination) pairs,
 and a starting airport, compute a possible itinerary. If no such itinerary exists, return None. All flights must be used
 in the itinerary.
 
@@ -17,14 +17,60 @@ Example2: [('SFO', 'COM'), ('COM', 'YYZ')] starting = 'COM'
 '''
 
 '''                             My Solution                              '''
+""" I am confused how this is a backtracking problem. Wouldn't I just start at the starting airport, go to its destination,
+and pop that tuple out of the list, then repeat until there is no match for starting airport or the list is empty in which
+case we are done? I do not see how I backtrack here? """
+from typing import List
+def itinerary(starting: str, org_dest: List[tuple], result=[]) -> List[str]:
+    try:
+        # Base Case
+        if not org_dest:
+            return result + [starting]
+
+        # Recursive Case
+        for org, dest in org_dest:
+            if org == starting:
+                org_dest.remove((org, dest))
+                return itinerary(dest, org_dest, result + [org])
+
+        # starting not any origin so can not complete itinerary
+        raise NoPossibleItinerary(starting, result)
+
+    except NoPossibleItinerary as e:
+        print("No possible itinerary exists.")
+        print(f"Current trip: {e.result_so_far}")
+        print(f'Got stuck in {e.current}')
+
+class NoPossibleItinerary(Exception):
+    def __init__(self, start: str, result: List[str]):
+        self.current = start
+        self.result_so_far = result + [start]
 
 
 '''                             Book Solution                            '''
+def get_itinerary(flights, current_itinerary):
+    if not flights:
+        return current_itinerary
 
+    last_stop = current_itinerary[-1]
+    for i, (origin, destination) in enumerate(flights):
+        flights_minus_current = flights[:i] + flights[i+1:]
+        current_itinerary.append(destination)
+        if origin == last_stop:
+            return get_itinerary(flights_minus_current, current_itinerary)
+        current_itinerary.pop()
+
+    return None
 
 '''                             Test Cases                               '''
 def main():
-    return
+    i1 = [('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')]
+    starting = 'YUL'
+    assert itinerary(starting, i1) == ['YUL', 'YYZ', 'SFO', 'HKO', 'ORD']
+
+    i2 = [('SFO', 'COM'), ('COM', 'YYZ')]
+    starting = 'COM'
+    itinerary(starting, i2)
 
 if __name__ == '__main__':
     main()
@@ -35,5 +81,11 @@ Lessons Learned:
         idea to let things fail silently.
     * Wouldn't this be a better example of backtracking if a starting airport like say 'SFO' had more than one destination
         airport so that if it doesnt find a path moving forward it can back track and try a new path?
+            No becuase all flights must be used...
+    * Create a class that inherits from Exception, then you can pass variables to it by using the dunder init
+        then use the try block on your code and raise this exception if needed and catch it in the except block
+        and print out whatever message you want.
+    * This felt like a real stretch for them to turn this problem into a backtracking problem. I just do not think this
+        is a good example of when to use backtracking.
 '''
 
